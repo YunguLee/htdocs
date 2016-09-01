426d6d8aa516e349a09cfe07ba3d4785428740db
YunguLee <aig211@naver.com>
2016-09-01 10:26:04 +0900

    첫번째 php

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+    phpinfo();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Today is first php</title>
+</head>
+<body>
+   <li>1</li>
+   <li>2</li>
+   <?php
+        print "SSDD";
+    ?>
+   <li>3</li>
+   <li>4</li>
+</body>
+</html>
