027b6557994604ac1150908981520d1fd5bb148a
YunguLee <aig211@naver.com>
2016-09-01 11:34:51 +0900

    첫번째 php

--- test.php
+++ test.php
@@ -8,6 +8,7 @@
     <title>Today is first php</title>
 </head>
 <body>
+  <ul>
    <li>1</li>
    <li>2</li>
    <?php
@@ -25,6 +26,45 @@
         print __DIR__;
        ?>
        </li>
-   <li>4</li>
+   <li>4
+   <?php
+        $data="SD";
+        $data1=1;
+        print $data;
+        print $data1;
+       
+        $week[]="Mon";
+        $week[]="Tue";
+            //같음
+        $week[0]="Mon";
+        $week[1]="Tue";
+        print $week[0];
+        print $week[1];
+        //array 함수
+       $Month=array(1,2,3,4,5,6,7,8,9,10,11,12,"Mon");
+       //5.4이상부터 사용가능
+       $year=[2016,2017];
+       //배열초기화
+       $month=array();
+       //연관배열
+       $member["name"]="GIC";
+       $member["age"]=3;
+       $member["height"]=135;
+            print $member["age"];
+            print_r ($member)."<br>";
+    ?></li>
+    </ul>
+    
+    
+    <p style="background-color: gray">
+    <?php
+    for ($k = 1 ; $k <10 ; $k++) {
+            for ($x = 1 ; $x <10 ; $x++){
+            $j = $x * $k;
+        echo "$k * $x = $j, ";
+        }
+    echo "<hr>";
+       }
+    ?>
 </body>
 </html>
