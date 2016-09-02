6a4519fde9630b0a943aa66cf7b9ca992ee90630
YunguLee <aig211@naver.com>
2016-09-02 11:36:35 +0900

    20160902

--- view.php
+++ view.php
@@ -6,7 +6,14 @@
 </head>
 <body>
     <?php
-    
+    print $_POST["onname"]."Welcome"."<br>";  
+    print nl2br($_POST["bonmun"]);
+    if(isset($_POST["hobby"])){
+        $hobby=implode('and'. $_POST["hobby"]);
+        print "my hobby is ".$hobby."pp";
+    }else{
+        print "i have no hobby";
+    }
     ?>
 </body>
 </html>
