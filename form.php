ea4f53f0441ffeab4e7810842b3defcbbf6f9503
YunguLee <aig211@naver.com>
2016-09-02 11:06:16 +0900

    20160902

---
+++ form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Text Sending Test</title>
+</head>
+<body>
+   <form name="form1" method="post" action="view.php">
+        name:<br>
+        <input type="text" name="onname"><br>
+        <input type="submit" value="sending"><br>
+    </form>
+</body>
+</html>
