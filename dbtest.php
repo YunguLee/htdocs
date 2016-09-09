1c74bef91c331e881074123dfb291e097e432f53
xkaxha123 <xkaxha123@naver.com>
2016-09-09 11:32:08 +0900

    201609

--- dbtest.php
+++ dbtest.php
@@ -6,10 +6,20 @@
 </head>
 <body>
     <?php
-    $con=mysql_connect('127.0.0.1', 'root', '') or die('Connect is Failed');
+    /*$con=mysql_connect('127.0.0.1', 'root', '') or die('Connect is Failed');
     print "Connect is Success";
     mysql_select_db('php');
-    mysql_close($con);
+    mysql_close($con);*/
+    /*
+    $mysqli=new mysqli('127.0.0.1', 'root', '','php');
+    if($mysqli->connect_error){
+    or die('connect is terminated');
+    }
+    print "connect is success";
+    $mysqli->close;*/
+    $pdo=new PDO('mysql:host=127.0.0.1;dbname=php;charset=utf8','root','')
+    print 'Connect is Success';
+    $pdo=null;
     ?>
 </body>
 </html>
