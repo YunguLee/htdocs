05e7992a37d8816326dbcefe65872bac45d21a99
YunguLee <aig211@naver.com>
2016-09-22 11:26:23 +0900

    20160922

--- list.php
+++ list.php
@@ -1,7 +1,7 @@
 <?php 
     $search='%'.$_POST['search'].'%';
     include 'kccdb.php';
-    $sql="select * from member where last_name like: last_name or first_name like: first-name";
+    $sql="select * from member where last_name like :last_name or first_name like :first-name";
     $stmh=$pdo->prepare($sql);
     $stmh->bindvalue(':last_name', $search, PDO::PARAM_STR);
     $stmh->excute();
